2470e91462b35232adaeb6dd0f7a22946140c9fc
Kansai-Student-Literary-Union <128477261+Kansai-Student-Literary-Union@users.noreply.github.com>
2023-07-30 18:11:22 +0900

    Update music.js

--- tohinmogi/hinaga/music.js
+++ tohinmogi/hinaga/music.js
@@ -2,7 +2,7 @@ function audio2() {
     document.getElementById('btn_audio2').currentTime = 0; //連続クリックに対応
     document.getElementById('btn_audio2').play(); //クリックしたら音を再生
 }
-function audio4() {
+function audio3() {
     document.getElementById('btn_audio3').currentTime = 0; //連続クリックに対応
     document.getElementById('btn_audio3').play(); //クリックしたら音を再生
 }
